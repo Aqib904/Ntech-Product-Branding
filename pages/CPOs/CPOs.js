a32ed73d99030f6e57779e2968fbdd16a87a9108
Aqib Siddique <74586324+Aqib904@users.noreply.github.com>
2024-02-14 22:41:44 +0500

    changes

--- pages/CPOs/CPOs.js
+++ pages/CPOs/CPOs.js
@@ -175,7 +175,7 @@ export default function CPOs() {
             <meta property="og:title" content={'Vehya'} />
             <meta
               property="og:description"
-              content={'Vehya is the premier app for electrical contractors and electricians looking to expand their services to the rapidly growing electric vehicle market. With the ability to serve customers globally and access to in-app training programs for everyone to become an EV service provider, you can be sure that you have the skills and knowledge necessary to provide top-notch service.'}
+              content={'Ntech Product Branding is the premier app for electrical contractors and electricians looking to expand their services to the rapidly growing electric vehicle market. With the ability to serve customers globally and access to in-app training programs for everyone to become an EV service provider, you can be sure that you have the skills and knowledge necessary to provide top-notch service.'}
             />
             <meta
               name="image" property="og:image"
--- pages/Utilities/Utilities.js
+++ pages/Utilities/Utilities.js
@@ -150,7 +150,7 @@ export default function Utilities() {
 					type: "article",
 					url: `https://aboutvehya.com/Utilities`,
 					title: 'Vehya',
-					description: 'Vehya is the premier app for electrical contractors and electricians looking to expand their services to the rapidly growing electric vehicle market. With the ability to serve customers globally and access to in-app training programs for everyone to become an EV service provider, you can be sure that you have the skills and knowledge necessary to provide top-notch service.',
+					description: 'Ntech Product Branding is the premier app for electrical contractors and electricians looking to expand their services to the rapidly growing electric vehicle market. With the ability to serve customers globally and access to in-app training programs for everyone to become an EV service provider, you can be sure that you have the skills and knowledge necessary to provide top-notch service.',
 					images: [
 						{
 							url: `https://www.dev.aboutvehya.com/logo1.png`,
